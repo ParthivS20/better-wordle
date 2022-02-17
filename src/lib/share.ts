69e46b86cbf2a2e0d4e55c7c17c23ea1bdacd941
Parthiv Shah <parthivjs12@gmail.com>
2022-02-16 21:12:36 -0600

    first

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,7 +1,6 @@
 import { getGuessStatuses } from './statuses'
 import { solutionIndex } from './words'
 import { GAME_TITLE } from '../constants/strings'
-import { getStoredIsHighContrastMode } from './localStorage'
 import { MAX_CHALLENGES } from '../constants/settings'
 
 export const shareStatus = (
@@ -21,25 +20,15 @@ export const generateEmojiGrid = (guesses: string[]) => {
   return guesses
     .map((guess) => {
       const status = getGuessStatuses(guess)
-      const isHighContrast = getStoredIsHighContrastMode()
       return guess
         .split('')
         .map((_, i) => {
           switch (status[i]) {
             case 'correct':
-              if (isHighContrast) {
-                return '🟧'
-              }
               return '🟩'
             case 'present':
-              if (isHighContrast) {
-                return '🟦'
-              }
               return '🟨'
             default:
-              if (localStorage.getItem('theme') === 'dark') {
-                return '⬛'
-              }
               return '⬜'
           }
         })
